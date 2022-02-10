615aec6f20aed3290e3181ceb7f723d9e548e26b
Tim <Tim280696@hotmail.com>
2022-02-10 12:55:03 +0100

    Updated style

--- src/Components/Todo/Todo.js
+++ src/Components/Todo/Todo.js
@@ -1,24 +1,39 @@
 import './todo.css'
 import iconCheck from './icon-check.svg'
+import { useState } from 'react'
 
 export default function Todo() {
+  const [todos, setTodos] = useState(['Faire des courses'])
+
   const handleSubmit = (e) => {
     e.preventDefault()
   }
   return (
-    <form className="todoContent">
-      <input
-        className="submitTodo"
-        type="submit"
-        value=" "
-        onClick={handleSubmit}
-      />
-      <input
-        type="text"
-        className="createTodo"
-        name="createTodo"
-        placeholder="Create a new todo..."
-      />
-    </form>
+    <>
+      <form className="todoForm">
+        <input
+          className="submitTodo"
+          type="submit"
+          value=" "
+          onClick={handleSubmit}
+        />
+        <input
+          type="text"
+          className="createTodo"
+          name="createTodo"
+          placeholder="Create a new todo..."
+        />
+      </form>
+      <div className="todoList">
+        {todos.map((todo) => {
+          return (
+            <div className="todoItem" key={todo}>
+              {todo}
+            </div>
+          )
+        })}
+        <div className="todoInfo">items left</div>
+      </div>
+    </>
   )
 }
